7980f99c39009560602daec5faa92d56c91f6023
utshab-47 <333uts@gmail.com>
2025-01-12 12:43:21 +0545

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:"/vite-react-deploy/";
+  base:"/vite-react-deploy/",
   server: {
     port: 5173,
     host: true
